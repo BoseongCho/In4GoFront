b3daccbbbd046b8f17902ad89f81e3d1e5b6bc4a
TonyChoil <creba1234@gmail.com>
2023-11-23 22:44:51 +0900

    App Routes 환경 세팅 완료 / index Provider, Store작성,  Navbar css + 뼈대 완성, Main화면 추가

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,5 +1,64 @@
-function Navbar(){
+import {Navigate, NavLink} from "react-router-dom";
+import NavCSS from "./Navbar.module.css";
+import {useNavigate} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
 
+
+function Navbar() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+    return (
+      <>
+        <div className={NavCSS.bwzYVU}>
+          <div className={NavCSS.bReTMH}>
+            <div className={NavCSS.cAnmmb}>
+              <div className={NavCSS.jqVsbC}>
+                <span>
+                  <div style={{width: "128px", height: "50px"}}>
+                    <NavLink to="/">
+                      <img
+                        src="./images/INSA PEOPLE.png"
+                        alt=""
+                        width={150}
+                        height={70}
+                      />
+                    </NavLink>
+                  </div>
+                </span>
+              </div>
+
+              <div className={NavCSS.lkOGXI}>
+                <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>
+                  <NavLink to="/">HOME</NavLink>
+                </span>
+
+                <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>
+                <NavLink to="/personnel">인사</NavLink>
+                 </span>
+
+                <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>
+                <NavLink to="/workmanage">근무/휴가</NavLink>
+                </span>
+
+                 <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>
+                     <NavLink to="/salary/salinfo">급여</NavLink>
+                 </span>
+
+                  <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>
+                     <NavLink to="/dailyWorker">일용직</NavLink>
+                  </span>
+                  <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>
+                    <NavLink to="/task">업무</NavLink>
+                  </span>
+                  <span className={`${NavCSS.fEXmlR} ${NavCSS.kOVHNW} ${NavCSS.nav}`}>게시판</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Navbar;
